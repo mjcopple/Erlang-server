9fa65095b8eda518b5eeea6774e95d16d5f7ba81
Micah Copple <micahcopple@letu.edu>
2009-12-06 19:50:16 -0600

    Format the IP address better.

--- ip.erl
+++ ip.erl
@@ -4,7 +4,7 @@
 
 get_ip_address_string() ->
 	{ok, IPAddress} = inet:getif(),
-	io_lib:write(filter_ip_address(IPAddress)).
+	filter_ip_address(IPAddress).
 
 filter_ip_address(IPAddress) ->
 	filter_ip_address(IPAddress, []).
@@ -15,5 +15,6 @@ filter_ip_address([{{127,0,0,1}, _Broadcast, _Mask} | Tail], Acc) ->
 	%Don't include the loopback address.
 	filter_ip_address(Tail, Acc);
 filter_ip_address([Head | Tail], Acc) ->
-{IP, _Broadcast, _Mask} = Head,
-	filter_ip_address(Tail, [IP | Acc]).
+	{{Oct1, Oct2, Oct3, Oct4}, _Broadcast, _Mask} = Head,
+	IPString = io_lib:format("~b.~b.~b.~b ", [Oct1, Oct2, Oct3, Oct4]),
+	filter_ip_address(Tail, IPString ++ Acc).
