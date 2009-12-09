6ad9dab1de735b4c597f1b94a43a6b2a1aeeab6f
Micah Copple <mjcopple@gmail.com>
2009-12-09 15:10:42 -0600

    Added doctype.

--- monitor.erl
+++ monitor.erl
@@ -4,7 +4,7 @@
 -export([start/0, all_known_nodes/0, get_time/0]).
 
 -define(Headers, "Content-Type: text/html\r\n\r\n").
--define(Top, "<html><head><link href=\"../../../styles.css\" rel=\"stylesheet\" type=\"text/css\"><title>Erlang cluster</title></head><body>").
+-define(Top, "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\"><html><head><link href=\"../../../styles.css\" rel=\"stylesheet\" type=\"text/css\"><title>Erlang cluster</title></head><body>").
 -define(Bottom, "</body></html>").
 
 -define(BackToMain, "<br><a href=main>Back to Main</a>").
